2ef489b63e19ac66c808fa67755d117f58d6469b
CR7 <123456789@qq.com>
2018-10-31 08:48:01 +0800

    aaa

--- demotest/src/main/java/cn/itcast/User.java
+++ demotest/src/main/java/cn/itcast/User.java
@@ -4,8 +4,5 @@ public class User {
 
     public static void main(String[] args) {
         System.out.println("Git");
-        System.out.println("Git2");
-        System.out.println("master");
-        System.out.println("dev");
     }
 }
